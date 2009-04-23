c866e8924da2326d5ff991f96743ba6492966e62
Mikhail Ivanov <ivmikhail@yaknet.com>
2009-04-23 14:55:41 +0000

    Готов загрузчик фото

--- src/AddPost.aspx
+++ src/AddPost.aspx
@@ -1,5 +1,7 @@
-<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AddPost.aspx.cs" Inherits="AddPost" Title="Ykt IT Community | Добавление новости" %>
+<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AddPost.aspx.cs" Inherits="AddPost" Title="Ykt IT Community | Добавление новости"  %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="server">
+
 <div id="add_post">
     <ul class="list">
         <li>
@@ -11,7 +13,7 @@
         <li>        
             <h2>
                 <label>
-                    Прикреплено <asp:CheckBox ID="CheckBoxAttached" runat="server" />
+                    Прикреплено <asp:CheckBox ID="CheckBoxAttached" runat="server" Enabled="false"/>
                 </label>
             </h2>
         </li>
@@ -28,30 +30,61 @@
             </label>
         </li>        
         <li>
-            <h2>Текст новости</h2>          
-            <label>               
-                <asp:TextBox ID="TextBoxText" runat="server" TextMode="MultiLine" Rows="25" Width="100%" MaxLength="2048"/>  
-            </label>
+            <h2>Текст новости</h2>      
+            <asp:TextBox ID="TextBoxText" runat="server" TextMode="MultiLine" Rows="25" Width="100%" MaxLength="2048"/>  
         </li>        
         <li>
             <h2>Источник(откуда стырили)</h2>
             <label>                
                 <asp:TextBox ID="TextBoxSource" runat="server" CssClass="input-text" MaxLength="256"/> 
             </label>
-        </li>    
-        <li>
-            <h2>Картинки</h2>
-            <label>
-               
-            </label>  
-        </li>
+        </li>            
         <li>
             <div class="error-message">
                 <asp:Literal ID="AddPostMessages" runat="server" />
             </div>
         </li>
-        <li>
+        <li style="text-align:right;">
             <asp:LinkButton ID="LinkButtonAdd" runat="server" OnClick="LinkButtonAdd_Click">Добавить</asp:LinkButton>
+        </li>
+        <li>
+            <script type="text/javascript">
+                function ReceiveServerData(rValue) 
+                {   
+                    if(rValue == "")
+                    {
+                        $('uploadImagesMessage').innerHTML = 'Картинка не прошла валидацию. Выберите другую.';
+                    } else
+                    {
+                        $('uploadImagesMessage').innerHTML = '';
+                        CreateNestedElements(rValue); 
+                    }
+                } 
+                function CreateNestedElements(newPath) 
+                {
+                    var imageObject = document.createElement('img'); 
+                    imageObject.src = newPath;                  
+                    $('uploadImagesList').appendChild(imageObject);   
+                }
+                function PopulateList(obj) 
+                { 
+                    CallServer(obj.value,'');    
+                }
+            </script>
+            <h2>Картинки</h2>
+                <p class="note">
+                        <asp:Literal ID="ImageOptions" runat="server"/>
+                </p>
+                <input id="list" type="hidden" runat="server"/>    
+                <h3>Выберите:</h3> 
+                <input id="image_upload" onchange="PopulateList(this)" name="image_upload" type="file" />  
+                
+                <h3>Загруженные:</h3>                     
+                <p class="note">                   
+                        Не добавленные в новость картинки будут удалены автоматически
+                </p>
+                <span id="uploadImagesMessage" class="error-message"></span>
+                <div id="uploadImagesList"></div>
         </li>
     </ul>
 </div>
--- src/AddPost.aspx.cs
+++ src/AddPost.aspx.cs
@@ -1,3 +1,4 @@
+/*
 using System;
 using System.Data;
 using System.Configuration;
@@ -9,16 +10,82 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Collections.Generic;
+ */
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
-public partial class AddPost : System.Web.UI.Page
+public partial class AddPost : System.Web.UI.Page, ICallbackEventHandler
 {
+    protected string returnValue = String.Empty;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
             LoadCategories();
+            if (User.IsInRole("1"))
+            {
+                CheckBoxAttached.Enabled = true;
+            }
+            ImageOptions.Text = Global.PostImageOptions;
+        }
+
+        /*
+        string[] filePaths = null;
+        if (!String.IsNullOrEmpty(list.Value))
+        {
+            filePaths = list.Value.Split('|');
+        }
+        */
+        // register the callback script 
+        string sbReference = ClientScript.GetCallbackEventReference(this, "arg", "ReceiveServerData", "context");
+        string cbScript = String.Empty;
+
+        // check if the script is already registered or not 
+        if (!ClientScript.IsClientScriptBlockRegistered("CallServer"))
+        {
+            cbScript = @" function CallServer(arg,context) { " + sbReference + "}";
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "CallServer", cbScript, true);
         }
     }
+
+    /// <summary>
+    /// Загружаем картинку
+    /// </summary>
+    /// <returns>Урл по которому доступна картинка</returns>
+    public string GetCallbackResult()
+    {
+        string result = "";
+        Picture pic = Picture.UploadImage(returnValue, CurrentUser.User.Id, 0);
+        if (pic.Name != "")
+        {
+            result = pic.ThumbUrl;
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Получаем что отправил нам клиент
+    /// </summary>
+    /// <param name="eventArgument">Путь к файлу на компе юзера</param>
+    public void RaiseCallbackEvent(string eventArgument)
+    {
+        if (!String.IsNullOrEmpty(eventArgument))
+        {
+            returnValue = eventArgument;
+        }
+    }
+
     private void LoadCategories()
     {
         List<Category> cats = Category.GetAll();
@@ -30,6 +97,8 @@ public partial class AddPost : System.Web.UI.Page
     }
     protected void LinkButtonAdd_Click(object sender, EventArgs e)
     {
+
+
         List<string> errors = ValidateData();
         if (errors.Count == 0)
         {
@@ -43,14 +112,16 @@ public partial class AddPost : System.Web.UI.Page
             newpost.Source = Server.HtmlEncode(TextBoxSource.Text);
             newpost.Author = CurrentUser.User;
             newpost.Attached = CheckBoxAttached.Checked;
+            Post current = Post.Add(newpost);
 
-            Post.Add(newpost);
-            Response.Redirect(FormsAuthentication.DefaultUrl);
+            //После добавления новости переименовываюм временную папку postimages/userid/0 в postimages/userid/postid
+            Picture.FixImages(current, CurrentUser.User.Id);
         } else
         {
-            WriteErrors(errors);
+            WriteErrors(errors, "Новость не добавлена");
         }
     }
+
     private List<string> ValidateData()
     {
         List<string> errors = new List<string>();
@@ -77,9 +148,9 @@ public partial class AddPost : System.Web.UI.Page
 
         return errors;
     }
-    private void WriteErrors(List<string> errors)
+    private void WriteErrors(List<string> errors, string title)
     {
-        string text = "<ul class='list'>";
+        string text = "<h2>" + title + "</h2><ul class='list'>";
         foreach (string message in errors)
         {
             text += "<li>" + message+"</li>";
