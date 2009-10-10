725fb36a12e3bee24e20ef1bf1b100016ce99694
Mikhail Ivanov <ivmikhail@gmail.com>
2009-10-10 06:23:24 +0000

    Косметические изменения, добавил новый бекап пустой бд(возможно что то изменил незначительно).
    Мигратор умеет тупо перетаскивать данные.

--- tools/datamigrator/datamigrator/Program.cs
+++ tools/datamigrator/datamigrator/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Data.SqlClient;
 using System.Data;
 using System.Data.Sql;
-using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -23,20 +22,28 @@ namespace datamigrator
             targetConn = OpenConnection("Data Source=localhost;Initial Catalog=itcommunity;Persist Security Info=True;User ID=wchk;Password=1234;persist security info=False;Connection Timeout=30;");
             sourceConn = OpenConnection("Data Source=localhost;Initial Catalog=itc;Persist Security Info=True;User ID=wchk;Password=1234;persist security info=False;Connection Timeout=30;");
 
-            WriteToLog("Ready, steady, GO!!!1");
-            MoveCategories();
-            MoveUsers();
-            MovePosts();
+            WriteToLog("INFO    Ready, steady, GO!!!1");
+
+            ClearTargetDB();
+           // MoveCategories();
+           // MoveUsers();
+           // MovePosts();
+           // MovePostComments();
 
             writer.Flush();
             writer.Close();
         }
 
+        private static void ClearTargetDB()
+        {
+            WriteToLog("INFO    Clearing target tables start...");
+            ClearTargetTables(new string[] { "categories", "users", "posts", "comments" });
+            WriteToLog("INFO    Clearing target tables end");
+        }
 
         private static void MoveCategories()
         {
-            WriteToLog("data migrating 'tblNewsType -> categories' start...");
-            ClearTargetTable("categories");
+            WriteToLog("INFO    data migrating 'tblNewsType -> categories' start...");
 
             DataTable sourceTable = GetSourceTable("tblNewsType");
             for (int i = 0; i < sourceTable.Rows.Count; i++ )
@@ -50,18 +57,17 @@ namespace datamigrator
             ExecuteQuery("SET IDENTITY_INSERT categories off", targetConn);
             ResetIdentitySeed("categories");
 
-            WriteToLog("data migrating 'tblNewsType -> categories' end");
+            WriteToLog("INFO    data migrating 'tblNewsType -> categories' end");
 
-            WriteToLog("data migrating 'post_cat' clearing...");
+            WriteToLog("INFO    data migrating 'post_cat' clearing...");
             ExecuteQuery("delete from post_cat", targetConn);
             ResetIdentitySeed("post_cat");
-            WriteToLog("data migrating 'post_cat' clearing end");
+            WriteToLog("INFO    data migrating 'post_cat' clearing end");
         }
         private static void MoveUsers()
         {
 
-            WriteToLog("data migrating 'tblUsers -> users' start...");
-            ClearTargetTable("users");
+            WriteToLog("INFO    data migrating 'tblUsers -> users' start...");
 
             DataTable sourceTable = GetSourceTable("tblUsers");
 
@@ -111,105 +117,165 @@ namespace datamigrator
 
                 if (currLogin != validLogin)
                 {
-                    WriteToLog("WARNING логин пользователя '" + currLogin + "' не валидный, теперь его логин - '" + validLogin + "'");
+                    WriteToLog("WARNING user login - '" + currLogin + "' is not valid, reset login to - '" + validLogin + "'");
+                    WriteToLog("WARNING all user(login - '" + currLogin + "') data mb will lost");
                 }
                 if (currEmail != validEmail)
                 {
                     if (currEmail == "")
                     {
-                        WriteToLog("WARNING емейл пользователя '" + currLogin + "' не задан(пустой), теперь его eмейл - '" + validEmail + "'");
+                        WriteToLog("WARNING user('" + currLogin + "') email not set(empty), reset email to - '" + validEmail + "'");
                     } else
                     {
-                        WriteToLog("WARNING емейл пользователя '" + currLogin + "' уже существует в базе('" + currEmail +"'), теперь его eмейл - '" + validEmail + "'");
+                        WriteToLog("WARNING user('" + currLogin + "') email('" + currEmail + "') already in db, reset email to - '" + validEmail + "'");
                     }
                   }
             }
 
             ExecuteQuery("SET IDENTITY_INSERT users off", targetConn);
             ExecuteQuery("update users set role = 2 where role = 3", targetConn);
+            ExecuteQuery("update users set role = 1 where nick = 'wchk'", targetConn); // НЕ СТИРАТЬ!!!!1
             ResetIdentitySeed("users");
-            WriteToLog("data migrating 'tblUsers -> users' end");
+            WriteToLog("INFO    data migrating 'tblUsers -> users' end");
         }
         private static void MovePosts()
         {
 
-            WriteToLog("data migrating 'tblNews -> posts' start...");
-            ClearTargetTable("posts");
+            WriteToLog("INFO    data migrating 'tblNews -> posts' start...");
 
             DataTable sourceTable = GetSourceTable("tblNews");
 
             for (int i = 0; i < sourceTable.Rows.Count; i++)
             {
-                SqlCommand cmd = new SqlCommand(
-                "SET IDENTITY_INSERT posts on;insert into posts(id,title,description,text,cdate,user_id,attached,views,source,comments_count) values(@id,@title,@description,@text,@cdate,@user_id,@attached,@views,@source,@comments_count)", targetConn);
-                
- 
-                SqlParameter id = cmd.Parameters.Add("@id", SqlDbType.Int);
-                id.Value = Convert.ToInt32(sourceTable.Rows[i]["news_id"]);
-                
-                SqlParameter title = cmd.Parameters.Add("@title", SqlDbType.NVarChar);
-                title.Value = sourceTable.Rows[i]["title"].ToString();
-                
-                SqlParameter desc = cmd.Parameters.Add("@description", SqlDbType.NVarChar);
-                desc.Value = sourceTable.Rows[i]["intro"].ToString();
-                
-                SqlParameter text = cmd.Parameters.Add("@text", SqlDbType.NVarChar);
-                text.Value = sourceTable.Rows[i]["main"].ToString();
-                
-                SqlParameter date = cmd.Parameters.Add("@cdate", SqlDbType.DateTime);
-                date.Value = Convert.ToDateTime(sourceTable.Rows[i]["ndate"].ToString());
-                
-                SqlParameter user_id = cmd.Parameters.Add("@user_id", SqlDbType.Int);
-                user_id.Value = ExecuteScalar("select id from users where UPPER(nick) = UPPER('" + sourceTable.Rows[i]["author"] + "')", targetConn);
-                
-                SqlParameter attached = cmd.Parameters.Add("@attached", SqlDbType.Int);
-                attached.Value = Convert.ToInt32(sourceTable.Rows[i]["ontop"].ToString());
-                
-                SqlParameter views = cmd.Parameters.Add("@views", SqlDbType.Int);
-                views.Value = Convert.ToInt32(sourceTable.Rows[i]["views"].ToString());
-                
-                SqlParameter source = cmd.Parameters.Add("@source", SqlDbType.NVarChar);
-                source.Value = sourceTable.Rows[i]["source"].ToString();
+                string author_login = sourceTable.Rows[i]["author"].ToString();
+                int author_id = ExecuteScalar("select id from users where UPPER(nick) = UPPER('" + author_login + "')", targetConn);
+                int post_id = Convert.ToInt32(sourceTable.Rows[i]["news_id"]);
+                if (author_id > 0)
+                {
+                    SqlCommand cmd = new SqlCommand(
+                    "SET IDENTITY_INSERT posts on;insert into posts(id,title,description,text,cdate,user_id,attached,views,source,comments_count) values(@id,@title,@description,@text,@cdate,@user_id,@attached,@views,@source,@comments_count)", targetConn);
 
-                SqlParameter comm_count = cmd.Parameters.Add("@comments_count", SqlDbType.Int);
-                comm_count.Value = ExecuteScalar("select count(*) from tblComment where cnews_id = '" + sourceTable.Rows[i]["news_id"] + "'", sourceConn);
 
-                cmd.ExecuteNonQuery();
-                /*
-                ExecuteQuery(@"SET IDENTITY_INSERT posts on; 
-                        insert into posts(id, 
-                                          title, 
-                                          description, 
-                                          text, 
-                                          cdate, 
-                                          user_id, 
-                                          attached, 
-                                          views, 
-                                          source,
-                                          comments_count) values("
-                        + sourceTable.Rows[i]["news_id"] + "," +
-                        "'" + sourceTable.Rows[i]["title"] + "'," +
-                        "'" + sourceTable.Rows[i]["intro"] + "'," +
-                        "'" + sourceTable.Rows[i]["main"] + "'," +
-                        "CONVERT(datetime, '" + sourceTable.Rows[i]["ndate"].ToString() + "', 104)," +
-                        ExecuteScalar("select id from users where UPPER(nick) = UPPER('" + sourceTable.Rows[i]["author"] + "')", targetConn) + "," +
-                        sourceTable.Rows[i]["ontop"] + "," +
-                        sourceTable.Rows[i]["views"] + "," +
-                        "'" + sourceTable.Rows[i]["source"] + "'," +
-                        +ExecuteScalar("select count(*) from tblComment where cnews_id = '" + sourceTable.Rows[i]["news_id"] + "'", sourceConn) + ")"
-                        , targetConn);
-                */
-                ExecuteQuery("insert into post_cat(post_id, cat_id) values(" + sourceTable.Rows[i]["news_id"] + ", " + sourceTable.Rows[i]["newstype_id"] + " )", targetConn);
+                    SqlParameter id = cmd.Parameters.Add("@id", SqlDbType.Int);
+                    id.Value = post_id;
+
+                    SqlParameter title = cmd.Parameters.Add("@title", SqlDbType.NVarChar);
+                    //обрезаем слижком длинные тайтлы
+                    string title_original = sourceTable.Rows[i]["title"].ToString();
+                    title.Value = title_original.Length > 128 ? title_original.Substring(0, 123) : title_original;
+
+                    SqlParameter desc = cmd.Parameters.Add("@description", SqlDbType.NVarChar);
+                    desc.Value = sourceTable.Rows[i]["intro"].ToString();
+
+                    SqlParameter text = cmd.Parameters.Add("@text", SqlDbType.NVarChar);
+                    text.Value = sourceTable.Rows[i]["main"].ToString();
+
+                    SqlParameter date = cmd.Parameters.Add("@cdate", SqlDbType.DateTime);
+                    date.Value = Convert.ToDateTime(sourceTable.Rows[i]["ndate"].ToString());
+
+                    SqlParameter user_id = cmd.Parameters.Add("@user_id", SqlDbType.Int);
+                    user_id.Value = author_id;
+
+                    SqlParameter attached = cmd.Parameters.Add("@attached", SqlDbType.Int);
+                    attached.Value = Convert.ToInt32(sourceTable.Rows[i]["ontop"].ToString());
+
+                    SqlParameter views = cmd.Parameters.Add("@views", SqlDbType.Int);
+                    views.Value = Convert.ToInt32(sourceTable.Rows[i]["views"].ToString());
+
+                    SqlParameter source = cmd.Parameters.Add("@source", SqlDbType.NVarChar);
+                    //обрезаем слижком длинные источники новостей
+                    string source_original = sourceTable.Rows[i]["source"].ToString();
+                    source.Value = source_original.Length > 1024 ? source_original.Substring(0, 123) : source_original;
+
+                    SqlParameter comm_count = cmd.Parameters.Add("@comments_count", SqlDbType.Int);
+                    comm_count.Value = ExecuteScalar("select count(*) from tblComment where cnews_id = '" + sourceTable.Rows[i]["news_id"] + "'", sourceConn);
+
+                    cmd.ExecuteNonQuery();
+
+                    ExecuteQuery("insert into post_cat(post_id, cat_id) values(" + sourceTable.Rows[i]["news_id"] + ", " + sourceTable.Rows[i]["newstype_id"] + " )", targetConn);
+                } else
+                {
+                    WriteToLog("WARNING cannot move post with id = '" + post_id + "': FK error, author(user) with nick = '" + author_login + "' not exist");
+                }
             }
             ExecuteQuery("SET IDENTITY_INSERT posts off", targetConn);
             ResetIdentitySeed("posts");
-            WriteToLog("data migrating 'tblNews -> posts' end");
+            WriteToLog("INFO    data migrating 'tblNews -> posts' end");
+        }
+        private static void MovePostComments()
+        {
+
+            WriteToLog("INFO    data migrating 'tblComment -> comments' start...");
+
+            DataTable sourceTable = GetSourceTable("tblComment");
+
+            for (int i = 0; i < sourceTable.Rows.Count; i++)
+            {
+                string author_login = sourceTable.Rows[i]["nick"].ToString();
+                int author_id = ExecuteScalar("select id from users where UPPER(nick) = UPPER('" + author_login + "')", targetConn);
+                int news_id = Convert.ToInt32(sourceTable.Rows[i]["cnews_id"]);
+                int comment_id = Convert.ToInt32(sourceTable.Rows[i]["comment_id"]);
+                bool is_post_exist = ExecuteScalar("select id from posts where id = " + news_id + " ", targetConn) > 0;
+                if (author_id > 0 && is_post_exist)
+                {
+                    SqlCommand cmd = new SqlCommand(
+                    "SET IDENTITY_INSERT comments on;insert into comments(id,post_id,user_id,cdate,ip,text) values(@id,@post_id,@user_id,@cdate,@ip,@text)", targetConn);
+
+
+                    SqlParameter id = cmd.Parameters.Add("@id", SqlDbType.Int);
+                    id.Value = comment_id;
+
+                    SqlParameter post_id = cmd.Parameters.Add("@post_id", SqlDbType.Int);
+                    post_id.Value = news_id;
+
+                    SqlParameter user_id = cmd.Parameters.Add("@user_id", SqlDbType.Int);
+                    user_id.Value = author_id;
+
+                    SqlParameter cdate = cmd.Parameters.Add("@cdate", SqlDbType.DateTime);
+                    cdate.Value = Convert.ToDateTime(sourceTable.Rows[i]["cdate"].ToString());
+
+                    SqlParameter ip = cmd.Parameters.Add("@ip", SqlDbType.NVarChar);
+                    ip.Value = sourceTable.Rows[i]["ip"].ToString();
+
+                    SqlParameter text = cmd.Parameters.Add("@text", SqlDbType.NVarChar);
+                    string comment_text = sourceTable.Rows[i]["comment"].ToString(); 
+                    text.Value = comment_text.Length > 512 ? comment_text.Substring(0, 512) : comment_text;
+
+                    cmd.ExecuteNonQuery();
+                } else
+                {
+                    if (is_post_exist)
+                    {
+                        WriteToLog("WARNING cannot move comment with id = '" + comment_id + "': FK error, post with id = '" + news_id + "' not exist");
+                    } else
+                    {
+                        WriteToLog("WARNING cannot move comment with id = '" + comment_id + "'(post id = '" + news_id + "'): FK error, comment author with login = '" + author_login + "' not exist");
+                    }
+                }
+            }
+            ExecuteQuery("SET IDENTITY_INSERT comments off", targetConn);
+            ResetIdentitySeed("comments");
+            WriteToLog("INFO    data migrating 'tblComments -> comments' end");
         }
 
 
 
-
         #region Всякая низкоуровневая хрень
+
+        private static void ClearTargetTables(string[] tables)
+        {
+            foreach (string tablename in tables)
+            {
+                WriteToLog("INFO    Clearing '" + tablename + "' start...");
+                ClearTargetTable(tablename);
+                WriteToLog("INFO    Clearing '" + tablename + "' end");
+            }
+        }
+
+        private static void ClearTargetTable(string tablename)
+        {
+            ExecuteQuery("delete from " + tablename, targetConn);
+        }
 
         private static bool IsLoginValid(string login)
         {
@@ -273,10 +339,7 @@ namespace datamigrator
             ExecuteQuery("DBCC CHECKIDENT ('" + tablename + "', RESEED, " + maxId + ") WITH NO_INFOMSGS;", targetConn);
 
         }
-        private static void ClearTargetTable(string tablename)
-        {
-            ExecuteQuery("delete from " + tablename, targetConn);
-        }
+
         private static void ExecuteQuery(string query, SqlConnection conn)
         {
             SqlCommand cmd = new SqlCommand(query, conn);
