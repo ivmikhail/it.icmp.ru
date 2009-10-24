55b11438803a7d2f2819db83d73807b67557ef09
Bald <bald2b@gmail.com>
2009-10-24 07:56:29 +0000

    1. Добавление новой Captcha не вызывает ошибки + некие исправления интерфейса 2. SendEmail SSL отключен 3. Убран олбанский кое-где 4. SQL-процедуры описаны в db/procs.sql

--- src/controls/ItCaptcha.ascx.cs
+++ src/controls/ItCaptcha.ascx.cs
@@ -23,7 +23,15 @@ namespace ITCommunity
         }
         private void setNewQuestion()
         {
-            VariantCaptcha ic = VariantCaptcha.GetItCaptcha();
+            VariantCaptcha ic = null;
+            int count = 0;
+            while (ic == null && count<10) {
+                ic = VariantCaptcha.GetItCaptcha();
+                count++;
+            }
+            if (ic == null) {
+                throw new Exception("cannot find right ItCaptcha");
+            }
             lblQuestion.Text = ic.Question;
             ddlVariants.Items.Clear();
             ddlVariants.Items.Add(new ListItem("Выберите вариант", "-1"));
