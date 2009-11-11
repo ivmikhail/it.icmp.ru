aa4ed90a48f25c11490fc16d1b1876e979397bd8
Mikhail Ivanov <ivmikhail@yaknet.com>
2009-11-11 11:05:40 +0000

    В догонку к r167

--- src/controls/menus/RedmineActivity.ascx.cs
+++ src/controls/menus/RedmineActivity.ascx.cs
@@ -14,117 +14,122 @@ using System.Diagnostics;
 using System.IO;
 using Rss;
 using System.Xml;
+
 namespace ITCommunity
 {
-	public partial class RedmineActivity : System.Web.UI.UserControl
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			int howItemsGet = Global.ConfigNumParam("RedmineActivityRssHowView");
-			List<RedmineActivityItem> items = loadDataFromRss(howItemsGet);
-			ActivityItems.DataSource = items;
-			ActivityItems.DataBind();
-		}
+    /**
+     Всякие методы типа загрузка из кеша, получение данных по rss нужно запихать в какой нибудь App_Code/RedmineActivity.cs
+     */ 
 
-		private List<RedmineActivityItem> loadDataFromRss(int howGet)
-		{
-			List<RedmineActivityItem> result = new List<RedmineActivityItem>();
-			String rssUrl = Global.ConfigStringParam("RedmineActivityRssUrl");
-			// не пашет
-			// AtomFeed feed = AtomFeed.Load(new Uri(rssUrl)); -
+    public partial class RedmineActivity : System.Web.UI.UserControl
+    {
+        //делегат метода загрузки данных
+        private delegate object RedmineActivityLoader(int howView);
+        private static readonly int cacheLiveTime = Global.ConfigNumParam("RedmineActivityCachePer");
+        private static readonly int howItemsGet = Global.ConfigNumParam("RedmineActivityRssHowView");
+        private static readonly String cacheKeyName = Global.ConfigStringParam("RedmineActivityCacheName");
 
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(rssUrl);
-			HttpWebResponse response = null;
-			try
-			{
-				response = (HttpWebResponse)request.GetResponse();
-				Stream stream = response.GetResponseStream();
-				XmlTextReader reader = new XmlTextReader(stream);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<RedmineActivityItem> items = loadDataFromCache(howItemsGet);
+            ActivityItems.DataSource = items;
+            ActivityItems.DataBind();
+        }
+        private List<RedmineActivityItem> loadDataFromCache(int howGet) {
+            RedmineActivityLoader loader = new RedmineActivityLoader(loadDataFromRss);
+            List<RedmineActivityItem> list = (List<RedmineActivityItem>)AppCache.Get(cacheKeyName, new object(), loader, new object[] { howGet }, DateTime.Now.AddMinutes(cacheLiveTime));
+            return list;
+        }
+        private List<RedmineActivityItem> loadDataFromRss(int howGet)
+        {
+            List<RedmineActivityItem> result = new List<RedmineActivityItem>();
+            String rssUrl = Global.ConfigStringParam("RedmineActivityRssUrl");
+            // не пашет
+            // AtomFeed feed = AtomFeed.Load(new Uri(rssUrl)); -
 
-				bool isTextTitle = false;
-				bool isTextAuthor = false;
-				bool isTextContent = false;
-				bool isTextUrl = false;
-				RedmineActivityItem item = null;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(rssUrl);
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                XmlTextReader reader = new XmlTextReader(stream);
 
-				while (reader.Read() && result.Count < howGet)
-				{
+                bool isTextTitle = false;
+                bool isTextAuthor = false;
+                bool isTextContent = false;
+                bool isTextUrl = false;
+                RedmineActivityItem item = null;
 
-					switch (reader.NodeType)
-					{
-						case XmlNodeType.Element:
-							if (reader.Name == "entry")
-							{
-								item = new RedmineActivityItem();
-								isTextTitle = false;
-								isTextAuthor = false;
-								isTextContent = false;
-								isTextUrl = false;
-							}
-							else if (reader.Name == "title")
-							{
-								isTextTitle = true;
-							}
-							else if (reader.Name == "author")
-							{
-								isTextAuthor = true;
-							}
-							else if (reader.Name == "content")
-							{
-								isTextContent = true;
-							}
-							else if (reader.Name == "id")
-							{
-								isTextUrl = true;
-							}
-							break;
-						case XmlNodeType.Text:
-							if (item != null)
-							{
-								if (isTextTitle)
-								{
-									item.Title = reader.Value;
-									isTextTitle = false;
-								}
-								else if (isTextAuthor)
-								{
-									item.Author = reader.Value;
-									isTextAuthor = false;
-								}
-								else if (isTextContent)
-								{
-									item.Text = reader.Value;
-									isTextContent = false;
-								}
-								else if (isTextUrl)
-								{
-									item.Url = reader.Value;
-									isTextUrl = false;
-								}
-							}
-							break;
-						case XmlNodeType.EndElement:
-							if (reader.Name == "entry")
-							{
-								result.Add(item);
-							}
-							break;
-					}
-				}
-				reader.Close();
-			}
-			catch (WebException ex)
-			{
-				Debug.Print(ex.Message);
-			}
-			finally
-			{
-				if (response != null)
-				{
-					response.Close();
-				}
-			}
-			return result;
-		}
-	}
+                while (reader.Read() && result.Count < howGet)
+                {
+
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name == "entry")
+                            {
+                                item = new RedmineActivityItem();
+                                isTextTitle = false;
+                                isTextAuthor = false;
+                                isTextContent = false;
+                                isTextUrl = false;
+                            } else if (reader.Name == "title")
+                            {
+                                isTextTitle = true;
+                            } else if (reader.Name == "author")
+                            {
+                                isTextAuthor = true;
+                            } else if (reader.Name == "content")
+                            {
+                                isTextContent = true;
+                            } else if (reader.Name == "id")
+                            {
+                                isTextUrl = true;
+                            }
+                            break;
+                        case XmlNodeType.Text:
+                            if (item != null)
+                            {
+                                if (isTextTitle)
+                                {
+                                    item.Title = HttpUtility.HtmlEncode(reader.Value);
+                                    isTextTitle = false;
+                                } else if (isTextAuthor)
+                                {
+                                    item.Author = HttpUtility.HtmlEncode(reader.Value);
+                                    isTextAuthor = false;
+                                } else if (isTextContent)
+                                {
+                                    item.Text = HttpUtility.HtmlEncode(reader.Value);
+                                    isTextContent = false;
+                                } else if (isTextUrl)
+                                {
+                                    item.Url = HttpUtility.HtmlEncode(reader.Value);
+                                    isTextUrl = false;
+                                }
+                            }
+                            break;
+                        case XmlNodeType.EndElement:
+                            if (reader.Name == "entry")
+                            {
+                                result.Add(item);
+                            }
+                            break;
+                    }
+                }
+                reader.Close();
+            } catch (WebException ex)
+            {
+                Logger.Log.Error("Не удалось получить активность redmine, rss fedd url - " + rssUrl, ex);
+            } finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+            return result;
+        }
+    }
 }
