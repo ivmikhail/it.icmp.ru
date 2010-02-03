3b29113ca8025c65cc8b2e7d130c052ebbb5137d
petch <zapetch@gmail.com>
2010-02-03 04:16:14 +0000

    Исправил кодировку MasterPage.master.cs; И стили немножко.

--- src/MasterPage.master.cs
+++ src/MasterPage.master.cs
@@ -11,37 +11,30 @@ using System.Web.UI.HtmlControls;
 using System.Collections.Generic;
 using ITCommunity;
 
-namespace ITCommunity
-{
-	public partial class MasterPage : System.Web.UI.MasterPage
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			if (!IsPostBack)
-			{
+namespace ITCommunity {
+	public partial class MasterPage : System.Web.UI.MasterPage {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (!IsPostBack) {
 				HeaderTextLiteral.Text = HeaderText.GetCurrent().Text;
 				ThisYear.Text = DateTime.Now.Year.ToString();
 			}
-            YaknetCounter.Text = "";
-            string url = Request.Url.ToString().ToLower();
-            if(url == "http://it.icmp.ru/" || url == "http://it.icmp.ru/default.aspx" || url == "http://it.icmp.ru/default.aspx?cat=0") {
-                YaknetCounter.Text= "<a href='http://www.ykt.ru/yaknet/default.asp'  title='Ïåðåéòè â ðåéòèíãîâóþ ñèñòåìó'><img src='http://www.ykt.ru/yaknet/image.asp?id=IT_community' alt='Ðåéòèíã Ykt.Ru' border='0' width='50' height='30'/></a>";
-            }
+			YaknetCounter.Text = "";
+			string url = Request.Url.ToString().ToLower();
+			if (url == "http://it.icmp.ru/" || url == "http://it.icmp.ru/default.aspx" || url == "http://it.icmp.ru/default.aspx?cat=0") {
+				YaknetCounter.Text = "<a href='http://www.ykt.ru/yaknet/default.asp' title='ÐŸÐµÑ€ÐµÐ¹Ñ‚Ð¸ Ð² Ñ€ÐµÐ¹Ñ‚Ð¸Ð½Ð³Ð¾Ð²ÑƒÑŽ ÑÐ¸ÑÑ‚ÐµÐ¼Ñƒ' id='yaknet-counter'><img src='http://www.ykt.ru/yaknet/image.asp?id=IT_community' alt='Ð ÐµÐ¹Ñ‚Ð¸Ð½Ð³ Ykt.Ru' border='0' width='50' height='30'/></a>";
+			}
 		}
 
-		private int GetCatId()
-		{
+		private int GetCatId() {
 			int id = 0;
 			Int32.TryParse(Request.QueryString["cat"], out id);
 			return id;
 		}
 
-		private int GetPageNum()
-		{
+		private int GetPageNum() {
 			int pageNum = 0;
 			Int32.TryParse(Request.QueryString["page"], out pageNum);
-			if (pageNum == 1)
-			{
+			if (pageNum == 1) {
 				pageNum = 0;
 			}
 			return pageNum;
