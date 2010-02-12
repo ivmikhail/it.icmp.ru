561a0b398f566cc409f19f077cecf665dec6a001
petch <zapetch@gmail.com>
2010-02-12 09:03:07 +0000

    Пока что реализовал просто + и -, можно оценивать сколько угодно. В прошлый раз забыл добавить Web.Config, там добавились несколько параметров для AJAX-оценки.

--- src/controls/Rating.ascx.cs
+++ src/controls/Rating.ascx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -8,12 +7,6 @@ using System.Web.UI.WebControls;
 namespace ITCommunity {
 	public partial class RatingControl : System.Web.UI.UserControl {
 		private Rating _rating = new Rating();
-
-		public int Id {
-			get {
-				return _rating.Id;
-			}
-		}
 
 		public int EntityId {
 			get {
@@ -42,8 +35,37 @@ namespace ITCommunity {
 		protected void Page_Load(object sender, EventArgs e) {
 			if (!IsPostBack) {
 				_rating = Rating.Get(EntityId, Type);
+				RatingUpdatePanel.DataBind();
 			}
 		}
 
+		protected void IncRatingClick(object sender, EventArgs e) {
+			AddRating(((LinkButton)sender).CommandArgument, true);
+		}
+
+		protected void DecRatingClick(object sender, EventArgs e) {
+			AddRating(((LinkButton)sender).CommandArgument, false);
+		}
+
+		private void AddRating(string commandArgument, bool isInc) {
+			if (!CurrentUser.isAuth) {
+				return;
+			}
+
+			string[] args = commandArgument.Split(',');
+			int entityId = Convert.ToInt32(args[0]);
+			EntityType entityType = (EntityType)Enum.Parse(typeof(EntityType), args[1]);
+			int value = isInc ? 1 : -1;
+			// Сюда писать формулы рейтингов
+			switch (entityType) {
+				case EntityType.Comment:
+				case EntityType.Post:
+				case EntityType.User:
+				default:
+					_rating = Rating.Add(entityId, entityType, CurrentUser.User.Id, value);
+					break;
+			}
+			RatingUpdatePanel.DataBind();
+		}
 	}
 }
