948aa6494b02ab952ff7c60fc0ed5edfe7255c75
petch <zapetch@gmail.com>
2010-02-16 05:57:48 +0000

    Забыл закоммитать базу.

--- db/rating.sql
+++ db/rating.sql
@@ -1,12 +1,9 @@
-/****** Object:  Table [dbo].[rating_logs]    Script Date: 02/11/2010 18:09:41 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[rating_logs]') AND type in (N'U'))
-DROP TABLE [dbo].[rating_logs]
-GO
-/****** Object:  Table [dbo].[ratings]    Script Date: 02/11/2010 18:09:41 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ratings]') AND type in (N'U'))
 DROP TABLE [dbo].[ratings]
 GO
-/****** Object:  Table [dbo].[ratings]    Script Date: 02/11/2010 18:09:41 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[rating_logs]') AND type in (N'U'))
+DROP TABLE [dbo].[rating_logs]
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -21,11 +18,10 @@ CREATE TABLE [dbo].[ratings](
  CONSTRAINT [PK_ratings] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 END
 GO
-/****** Object:  Table [dbo].[rating_logs]    Script Date: 02/11/2010 18:09:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -38,11 +34,10 @@ CREATE TABLE [dbo].[rating_logs](
 	[entity_type] [int] NOT NULL,
 	[user_id] [int] NOT NULL,
 	[value] [int] NOT NULL,
-	[cdate] [datetime] NOT NULL,
+	[cdate] [datetime] NOT NULL CONSTRAINT [DF_rating_logs_cdate]  DEFAULT (getdate()),
  CONSTRAINT [PK_rating_logs] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 END
-GO
