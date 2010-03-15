e290d7c55abf7a9a3c463509a37c9fefe7868aa4
Mikhail Ivanov <ivmikhail@yaknet.com>
2010-03-15 11:20:38 +0000

    Улучшение в удобстве использования pop-up картинок

--- src/media/js/popup.js
+++ src/media/js/popup.js
@@ -1,4 +1,3 @@
 function popup(imgUrl) {
-    var isIE = (navigator.appName == "Microsoft Internet Explorer");
-    window.open("popup.html?" + imgUrl, 'ImageWindow', 'top=50,left=50,width=250,height=250,toolbar=no,menubar=no,scrollbars=no,resizable=yes');
+    window.open("popup.html?" + imgUrl, "ImageWindow", "top=50,left=50,width=250,height=250,toolbar=no,menubar=no,scrollbars=no,location=yes,resizable=yes");
 }
