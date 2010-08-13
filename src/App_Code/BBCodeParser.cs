2b0ee2f9d699216aacb7b1f472b73e4f8e69c271
petch <zapetch@gmail.com>
2010-08-13 01:23:33 +0000

    fixed #229, #241

--- src/App_Code/BBCodeParser.cs
+++ src/App_Code/BBCodeParser.cs
@@ -125,8 +125,8 @@ namespace ITCommunity {
 			imgPattern = imgPattern.Substring(0, imgPattern.Length - 1);
 			imgPattern += "))/[^ \"'\\[]*?)";
 			_formatters.Add(new RegexFormatter(@"\[img\]" + imgPattern + @"\[/img\]", "<img src=\"$1\" border=\"0\" alt=\"\" class=\"bbcode-img\" />"));
-			_formatters.Add(new RegexFormatter(@"\[img align=((.|\n)*?)\]" + imgPattern + @"\[/img\]", "<img src=\"$2\" border=\"0\" align=\"$1\" alt=\"\" class=\"bbcode-img align-$1\" />"));
-			_formatters.Add(new RegexFormatter(@"\[img=((.|\n)*?)x((.|\n)*?)\]" + imgPattern + @"\[/img\]", "<img width=\"$1\" height=\"$3\" src=\"$4\" border=\"0\" alt=\"\" class=\"bbcode-img\" />"));
+			_formatters.Add(new RegexFormatter(@"\[img align=((.|\n)*?)\]" + imgPattern + @"\[/img\]", "<img src=\"$3\" border=\"0\" align=\"$1\" alt=\"\" class=\"bbcode-img align-$1\" />"));
+			_formatters.Add(new RegexFormatter(@"\[img=((.|\n)*?)x((.|\n)*?)px\]" + imgPattern + @"\[/img\]", "<img width=\"$1px\" height=\"$3px\" src=\"$5\" border=\"0\" alt=\"\" class=\"bbcode-img\" />"));
 			//_formatters.Add(new RegexFormatter(@"\[img\]((.|\n)*?)\[/img\]", "<img src=\"$1\" border=\"0\" alt=\"\" class=\"bbcode-img\" />"));
 			//_formatters.Add(new RegexFormatter(@"\[img align=((.|\n)*?)\]((.|\n)*?)\[/img\]", "<img src=\"$3\" border=\"0\" align=\"$1\" alt=\"\" class=\"bbcode-img align-$1\" />"));
 			//_formatters.Add(new RegexFormatter(@"\[img=((.|\n)*?)x((.|\n)*?)\]((.|\n)*?)\[/img\]", "<img width=\"$1\" height=\"$3\" src=\"$5\" border=\"0\" alt=\"\" class=\"bbcode-img\" />"));
