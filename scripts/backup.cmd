876467856f3e07b6e2b333ead0b303b28aa79a64
Bald <bald2b@gmail.com>
2010-11-04 09:08:34 +0000

    Скрипты бекапа и деплоинга

---
+++ scripts/backup.cmd
@@ -0,0 +1,17 @@
+@echo off
+x:
+cd \www\itc-mvc-builder
+
+"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd" -S itcommunity\itc -d itc-mvc -i backup.sql
+
+rar a y:\backup\sql it_backup.trn -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
+sc stop redmine
+rar a y:\backup\redmine x:\www\redmine -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
+sc start redmine
+
+sc stop svn
+rar a y:\backup\repo x:\repo -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
+sc start svn
+
+rar a y:\backup\www x:\www\itc-mvc -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
+
---
+++ scripts/backup.sql
@@ -0,0 +1,3 @@
+BACKUP DATABASE [itc-mvc]
+ TO DISK = 'x:\www\itc-mvc-builder\it_backup.trn'
+   WITH FORMAT;
