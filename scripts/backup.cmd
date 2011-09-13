3739dc394ba9aae1d25d32c2dcf2eaf5459ae3bc
Mikhail Ivanov <ivmikhail@gmail.com>
2011-09-13 01:32:34 +0000

    добавил больше trusted сайтов для картинок

--- scripts/backup.cmd
+++ scripts/backup.cmd
@@ -1,10 +1,10 @@
 @echo off
 x:
-cd \www\itc-mvc-builder
+cd \www\itc-mvc-builder\scripts
 
 "C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd" -S itcommunity\itc -d itc-mvc -i backup.sql
 
-rar a y:\backup\sql it_backup.trn -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
+rar a y:\backup\sql ..\it_backup.trn -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
 sc stop redmine
 rar a y:\backup\redmine x:\www\redmine -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
 sc start redmine
@@ -15,3 +15,4 @@ sc start svn
 
 rar a y:\backup\www x:\www\itc-mvc -x@ignore.txt -r -ag-yyyy-mm-dd -s -t
 
+call start_services.cmd
--- scripts/build&copy.cmd
+++ scripts/build&copy.cmd
@@ -1,17 +1,23 @@
 @echo off
 x:
-cd \www\it-mvc-builder
-rd /s /q x:\www\itc-mvc\bin
+cd \www\itc-mvc-builder
+
 svn up .
+
+"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd" -S itcommunity\itc -d itc-mvc -i db\procs.sql
+
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild /p:OutDir=..\PrecompiledWeb\
 if %errorlevel%==0 goto copying
 
 :error
 echo Error building
+goto exit
 
 :copying
 echo ok
 
+rd /s /q x:\www\itc-mvc\bin
 del PrecompiledWeb\_PublishedWebsites\ITCommunity\web.config
 del PrecompiledWeb\_PublishedWebsites\ITCommunity\app.config
 xcopy /y /s PrecompiledWeb\_PublishedWebsites\ITCommunity\* x:\www\itc-mvc
+:exit
